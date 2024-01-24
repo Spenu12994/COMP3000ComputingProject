219517798c8d72f538241bce45c4833a33ba625c
Spenu12994 <91668500+Spenu12994@users.noreply.github.com>
2024-01-24 16:43:12 +0000

    Initial setup of piServer.py
    piServer.py is a server file that will be ran on our raspberry pi and host our webserver/webpage. temporary variables and functions are added to simulate

---
+++ piServer.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import render_template
+from gpiozero import Motor
+import RPI.GPIO as rpi
+import time
+
+#https://dev.to/alanjc/water-your-plant-using-a-raspberry-pi-and-python-2ddb
+#https://projects.raspberrypi.org/en/projects/physical-computing/14
+#https://sourceforge.net/p/raspberry-gpio-python/wiki/Examples/
+#https://www.hackster.io/adhyoksh/controlling-gpio-pins-of-raspberry-pi-with-web-page-2d5bdc
+
+
+app=Flask(__name__)
+
+#setup pins
+watSen, lighSen= 3, 5
+
+rpi.setwarning(False)
+rpi.setmode(rpi.BOARD)
+
+#rpi setup our water sensor and ligh sensor with their pins
+rpi.setup(watSen, rpi.IN)
+rpi.setup(lighSen, rpi.IN)
+
+#change 7 and 14 for our pins
+pump = Motor(7, 14)
+#use commands like pump.forward() pump.backward(), pump.stop()
+
+waterLevel = rpi.input(watSen)
+lightLevel = rpi.input(lighSen)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/waterLevel')
+def index():
+    waterLevel = rpi.input(watSen)
+    return render_template('index.html')
+
+@app.route('/lightLevel')
+def index():
+    lightLevel = rpi.input(lighSen)
+    return render_template('index.html')
+
+@app.route('/water')
+def index():
+    #code to water plants
+    pump.forward(0.5) #activate pump at half speed
+    sleep(5) #wait for 5
+    pump.stop() #stop the pump
+
+    return render_template('index.html')
